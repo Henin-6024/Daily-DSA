c39e2386fa01ee526d4b416acf2e46a09a7913e8
Henin  Kamli <128271254+Henin-6024@users.noreply.github.com>
2024-02-26 20:46:34 +0530

    Create 100. Same Tree.py

---
+++ 100. Same Tree.py
@@ -0,0 +1,20 @@
+# 26 POTD 
+# Problem: 100. Same Tree
+# Language :  python3 
+# Link:  https://leetcode.com/problems/same-tree/submissions/1186836230
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p == q:
+            return True
+        if p is None or q is None or p.val != q.val :
+            return False
+        return self.isSameTree(p.left, q.left) and \
+               self.isSameTree(p.right, q.right)
