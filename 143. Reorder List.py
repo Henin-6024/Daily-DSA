5050f79233e510a46ef5b976e422b27e42ebc9e1
Henin  Kamli <128271254+Henin-6024@users.noreply.github.com>
2024-03-23 23:05:46 +0530

    Create 143. Reorder List.py

---
+++ 143. Reorder List.py
@@ -0,0 +1,41 @@
+# 26 POTD 
+# Problem: 143. Reorder List
+# Language :  python3 
+# Link: https://leetcode.com/problems/reorder-list/submissions/1211828217
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next 
+            fast = fast.next.next
+        
+        mid = slow
+        first = head
+        curr, prev = mid, None
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        second = prev
+        while second.next:
+            temp1 = first.next
+            first.next = second
+            first = temp1
+
+            temp2 = second.next
+            second.next = first
+            second = temp2
+            
+
+
+        
