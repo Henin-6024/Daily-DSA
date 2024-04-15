31e14ce57bd64f68d5f6a69dcfaea8efa716cffb
Henin  Kamli <128271254+Henin-6024@users.noreply.github.com>
2024-04-15 22:13:32 +0530

    Create 129. Sum Root to Leaf Numbers.java

---
+++ 129. Sum Root to Leaf Numbers.java
@@ -0,0 +1,40 @@
+# 15 POTD 
+# Problem: 129. Sum Root to Leaf Numbers
+# Language :  JAVA 
+# Link: https://leetcode.com/problems/sum-root-to-leaf-numbers/submissions/1233217476
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        dfs(root, 0);
+        return ans;
+    }
+    private int ans = 0;
+    private void dfs(TreeNode root, int path)
+    {
+        if(root == null) return;
+        if(root.left == null && root.right == null){
+            ans += path * 10 + root.val;
+            return;
+        }
+        dfs(root.left, path * 10 + root.val);
+        dfs(root.right, path * 10 + root.val);
+     }
+
+
+
+}
