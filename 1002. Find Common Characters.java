fd789807246b381af53cb66a416fefc4edaa2ab0
Henin  Kamli <128271254+Henin-6024@users.noreply.github.com>
2024-06-05 22:06:27 +0530

    Create 1002. Find Common Characters.java

---
+++ 1002. Find Common Characters.java
@@ -0,0 +1,29 @@
+/*
+# 05 POTD 
+# Problem: 1002. Find Common Characters
+# Language: Java
+# Link: https://leetcode.com/problems/find-common-characters/submissions/1278646352
+*/
+class Solution {
+  public List<String> commonChars(String[] A) {
+    List<String> ans = new ArrayList<>();
+    int[] commonCount = new int[26];
+    Arrays.fill(commonCount, Integer.MAX_VALUE);
+
+    for (String a : A) {
+      int[] count = new int[26];
+      for (char c : a.toCharArray())
+        ++count[c - 'a'];
+      for (int i = 0; i < 26; ++i)
+        commonCount[i] = Math.min(commonCount[i], count[i]);
+    }
+
+    for (char c = 'a'; c <= 'z'; ++c)
+      for (int i = 0; i < commonCount[c - 'a']; ++i)
+        ans.add(String.valueOf(c));
+
+    return ans;
+  }
+}
+
+
